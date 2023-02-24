ac50b13be3b6bcbc54f10e12fdf1aa652711e5f4
Ivan Matias <ivanmatias1331@gmail.com>
2023-02-24 00:41:39 -0800

    Adjusted item presentation on the store page

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -4,11 +4,11 @@ export default function ItemCard({item}) {
     console.log(item)
     return(
         <div className="itemCardContainer">
-            <div className="itemCardImage">
+            <Link to={`/store/${item.id}`} className="itemCardImage">
                 <img src={item.images[0]} alt={item.name} />
-            </div>
+            </Link>
             <div className="itemCardInfo">
-                <Link className="" to={`/store/${item.id}`}>{item.name}</Link>
+                <Link className="itemLink" to={`/store/${item.id}`}>{item.name}</Link>
                 {item.on_sale ? 
                     <div className="itemOnSale">
                         <p className="saleOriginalPrice">${item.stock_price}</p>
